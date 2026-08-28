f3b0046a1493896d98da46fa5470f26055fdf5c1
Fran Sarciat <fran@sarc-iat.com>
2026-08-28 17:25:27 +0200

    chore: add safe fallback values for supabase client

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://wjnoawmefdurqqjwqdmi.supabase.co";
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "sb_publishable_dWudcdKMOeKH22g0IRKV7w_bxWNtEh2";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
